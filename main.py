5bbcb24b06cfdf7160ea24b679c35d7ada58b933
Chen Jiahao <90749521+jiahaochen1015@users.noreply.github.com>
2022-09-27 17:16:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_birthday():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
-def get_words1():
+def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
@@ -48,6 +48,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature, wind = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"wind":{"value":wind},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words1()},"color":{"value":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"wind":{"value":wind},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words()},"color":get_random_color()}
 res = wm.send_template(user_id, template_id, data)
 print(res)
